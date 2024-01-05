fad823c971e88af6e04f166bada9b9a02488f5b3
sadfox1 <wildtorro2000@gmail.com>
2024-01-06 02:26:54 +0300

    modified:   bd.json 	modified:   othetSc.js

--- othetSc.js
+++ othetSc.js
@@ -1,15 +1,33 @@
-let backBTN = document.getElementById("backBTN");
+//let backBTN = document.getElementById("backBTN");
 
-const obj = {
-    "c" : "hui",
-    "r" : "hrew",
-    "r" : "hrew"
+let obj = {
+     "c" : "hui",
+     "r" : "hrew",
+     "g" : "hrew"
 }
 
-backBTN.addEventListener("click", function(e){
-    window.location.href = 'index.html';
+// backBTN.addEventListener("click", function(e){
+//     window.location.href = 'index.html';
+// });
+
+// подключаем модуль для работы с файловой системой
+const fs = require('fs');
+
+// данные, которые нужно сохранить в файл
+// имя файла, в который нужно сохранить данные
+const fileName = 'bd.json';
+obj = {
+    "c" :  Math.random(),
+    "r" : Math.random(),
+    "g" : Math.random()
+}
+
+// вызываем метод writeFile для записи данных в файл
+fs.writeFile(fileName, JSON.stringify(obj), (err) => {
+  // если произошла ошибка, выбрасываем исключение
+  if (err) throw err;
+  // выводим сообщение об успешной записи
+  console.log('Данные сохранены в файл');
 });
-
-
-
+console.log("Мб записалось");
 
